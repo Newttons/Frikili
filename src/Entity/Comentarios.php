eb6ab0c1a35df61b449e69800a7e0ca65daeded4
Newttons <padguille15@gmail.com>
2022-02-15 23:07:55 -0600

    Added Entities and template register

---
+++ src/Entity/Comentarios.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComentariosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ComentariosRepository::class)
+ */
+class Comentarios
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=80000, nullable=true)
+     */
+    private $comentario;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="comentarios")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Posts", inversedBy="comentarios")
+     */
+    private $posts;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComentario(): ?string
+    {
+        return $this->comentario;
+    }
+
+    public function setComentario(?string $comentario): self
+    {
+        $this->comentario = $comentario;
+
+        return $this;
+    }
+}
